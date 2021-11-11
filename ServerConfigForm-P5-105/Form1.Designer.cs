6025a6e6c6439912cc2f4d8d7415ed02baa5d399
Pahlevi Pasha <pahlevi.pasha09@gmail.com>
2021-11-11 17:54:55 +0700

    Add project files.

---
+++ ServerConfigForm-P5-105/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace ServerConfigForm_P5_105
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOn = new System.Windows.Forms.Button();
+            this.buttonOFF = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelKet = new System.Windows.Forms.Label();
+            this.labelonoroff = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonOn
+            // 
+            this.buttonOn.Location = new System.Drawing.Point(97, 261);
+            this.buttonOn.Name = "buttonOn";
+            this.buttonOn.Size = new System.Drawing.Size(75, 23);
+            this.buttonOn.TabIndex = 0;
+            this.buttonOn.Text = "ON";
+            this.buttonOn.UseVisualStyleBackColor = true;
+            this.buttonOn.Click += new System.EventHandler(this.buttonOn_Click);
+            // 
+            // buttonOFF
+            // 
+            this.buttonOFF.Location = new System.Drawing.Point(272, 261);
+            this.buttonOFF.Name = "buttonOFF";
+            this.buttonOFF.Size = new System.Drawing.Size(75, 23);
+            this.buttonOFF.TabIndex = 1;
+            this.buttonOFF.Text = "OFF";
+            this.buttonOFF.UseVisualStyleBackColor = true;
+            this.buttonOFF.Click += new System.EventHandler(this.buttonOFF_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(202, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Server";
+            // 
+            // labelKet
+            // 
+            this.labelKet.AutoSize = true;
+            this.labelKet.Location = new System.Drawing.Point(205, 155);
+            this.labelKet.Name = "labelKet";
+            this.labelKet.Size = new System.Drawing.Size(35, 13);
+            this.labelKet.TabIndex = 3;
+            this.labelKet.Text = "label2";
+            // 
+            // labelonoroff
+            // 
+            this.labelonoroff.AutoSize = true;
+            this.labelonoroff.Location = new System.Drawing.Point(205, 185);
+            this.labelonoroff.Name = "labelonoroff";
+            this.labelonoroff.Size = new System.Drawing.Size(35, 13);
+            this.labelonoroff.TabIndex = 4;
+            this.labelonoroff.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 406);
+            this.Controls.Add(this.labelonoroff);
+            this.Controls.Add(this.labelKet);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonOFF);
+            this.Controls.Add(this.buttonOn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOn;
+        private System.Windows.Forms.Button buttonOFF;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelKet;
+        private System.Windows.Forms.Label labelonoroff;
+    }
+}
+
---
+++ ServerConfigForm-P5-105/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceModel;
+using ServiceMtk_P1_NIM;
+
+namespace ServerConfigForm_P5_105
+{
+    public partial class Form1 : Form
+    {
+        private ServiceHost Host;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOn_Click(object sender, EventArgs e)
+        {
+            Host = new ServiceHost(typeof(Matematika));
+
+            labelonoroff.Text = "Server ON";
+            labelKet.Text = "Klik OFF untuk Mematikan Server";
+            Host.Open();
+            buttonOn.Enabled = false;
+            buttonOFF.Enabled = true;
+        }
+
+        private void buttonOFF_Click(object sender, EventArgs e)
+        {
+            labelonoroff.Text = "Server OFF";
+            labelKet.Text = "Klik ON untuk Menjalankan Server";
+            Host.Close();
+            buttonOn.Enabled = true;
+            buttonOFF.Enabled = false;
+        }
+    }
+}
